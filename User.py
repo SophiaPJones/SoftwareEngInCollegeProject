83813b5a9960847f692b04893ca28e06d6b04c96
Sophia Jones <sophiapjones23@gmail.com>
2023-02-07 18:03:37 -0500

    Update User.py

--- User.py
+++ User.py
@@ -9,3 +9,10 @@ class User:
         return f"{self.username},{self.password}";
     def change_password(self, new_password):
         self.password = new_password
+    def save_account(self):
+        account = (self.username, self.password)
+        with open('accounts.csv','a') as target:
+            writer = csv.writer(target)
+            writer.writerow(account)
+        target.close()
+  
