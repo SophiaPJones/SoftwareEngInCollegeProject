c6fe3c07fc1c96b086c9af33b90a538210470bf3
Ty Desktop <typiesco@gmail.com>
2023-02-17 15:25:28 -0500

    Bug fix + test fix

--- State.py
+++ State.py
@@ -16,33 +16,37 @@ class State:
         self.root = None
         self.jobs = []
         self.success_stories = {}
+
     def load_accounts(self):
         try:
-            with open(self.account_file_name) as csvFile:
+            with open(self.account_file_name, encoding="utf8") as csvFile:
                 lines = list(line for line in (l.strip()
                              for l in csvFile) if line)  # skip blank lines
                 accountList = csv.reader(lines, delimiter=',')
                 for account in accountList:
                     self.users[account[2]] = User.User(
-                        account[0], #first name
-                        account[1], #last name
-                        account[2], #username
-                        account[3]) #password
+                        account[0],  # first name
+                        account[1],  # last name
+                        account[2],  # username
+                        account[3])  # password
                     account[4] = account[4].replace('\u2063', '\n')
                     account[4] = account[4].replace('\u2064', ',')
-                    self.users[account[2]].set_success_story(account[4]) #success story
+                    self.users[account[2]].set_success_story(
+                        account[4])  # success story
 
                 pass
         except:
             return
+
     def save_accounts(self):
         if len(self.users) <= Util.MAXIMUM_USER_COUNT:
-            with open(self.account_file_name, 'w') as target:
+            with open(self.account_file_name, 'w', encoding="utf8") as target:
                 writer = csv.writer(target)
                 for account in list(self.users.keys()):
                     accountlist = self.users[account].list()
-                    accountlist[4] = accountlist[4].replace('\n','\u2063') #replacing newlines and commas with uncommon alternative separator characters
-                    accountlist[4] = accountlist[4].replace(',','\u2064')
+                    # replacing newlines and commas with uncommon alternative separator characters
+                    accountlist[4] = accountlist[4].replace('\n', '\u2063')
+                    accountlist[4] = accountlist[4].replace(',', '\u2064')
                     writer.writerow(accountlist)
 
             target.close()
@@ -72,11 +76,11 @@ class State:
                 job_list = csv.reader(lines, delimiter=',')
                 for job in job_list:
                     self.jobs.append(Job.Job(
-                        job[0], #title
-                        job[1], #description
-                        job[2], #employer
-                        job[3], #location
-                        job[4] #salary
+                        job[0],  # title
+                        job[1],  # description
+                        job[2],  # employer
+                        job[3],  # location
+                        job[4]  # salary
                     ))
                 pass
         except:
@@ -84,4 +88,5 @@ class State:
 
     def load_success_stories(self):
         if self.users != {}:
-            self.success_stories = {key: val.success_story for key, val in self.users.items() if val.success_story != ""}
+            self.success_stories = {key: val.success_story for key,
+                                    val in self.users.items() if val.success_story != ""}
