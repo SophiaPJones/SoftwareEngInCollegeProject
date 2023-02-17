def0edce7bf822e66b3d2f655b2e4e1881c6cfd7
Ty Desktop <typiesco@gmail.com>
2023-02-17 12:22:34 -0500

    Fixed failing tests

--- test_state.py
+++ test_state.py
@@ -6,7 +6,8 @@ def test_saving_account():
     newState = State()
 
     newState.account_file_name = "accounts.csv"
-    newState.users["UserName1"] = User("Username1", "Password1!")
+    newState.users["UserName1"] = User(
+        "UserFirst", "UserLast", "Username1", "Password1!")
     assert newState.save_accounts() == True
 
 
@@ -14,11 +15,17 @@ def test_saving_over_five_accounts():
     newState = State()
 
     newState.account_file_name = "accounts.csv"
-    newState.users["UserName1"] = User("Username1", "Password1!")
-    newState.users["UserName2"] = User("Username2", "Password1!")
-    newState.users["UserName3"] = User("Username3", "Password1!")
-    newState.users["UserName4"] = User("Username4", "Password1!")
-    newState.users["UserName5"] = User("Username5", "Password1!")
-    newState.users["UserName6"] = User("Username6", "Password1!")
+    newState.users["UserName1"] = User(
+        "UserFirst", "UserLast", "Username1", "Password1!")
+    newState.users["UserName2"] = User(
+        "UserFirst", "UserLast", "Username2", "Password1!")
+    newState.users["UserName3"] = User(
+        "UserFirst", "UserLast", "Username3", "Password1!")
+    newState.users["UserName4"] = User(
+        "UserFirst", "UserLast", "Username4", "Password1!")
+    newState.users["UserName5"] = User(
+        "UserFirst", "UserLast", "Username5", "Password1!")
+    newState.users["UserName6"] = User(
+        "UserFirst", "UserLast", "Username6", "Password1!")
     newState.save_accounts()
     assert newState.save_accounts() == False
