deb1570c8878ab4f3bc22c5ab0845a32caf3845b
Sophia Jones <sophiapjones23@gmail.com>
2023-02-27 16:02:24 -0500

    Adds test cases for friend request funcitonality and user search functionality

--- test_StateTestCases.py
+++ test_StateTestCases.py
@@ -1,21 +1,41 @@
 from unittest import TestCase
+import pytest
+from os import *
+import io
+from Pages import SearchStudents
 from State import *
 from User import *
 from Util import *
 from Job import *
 
+@pytest.fixture(scope="function")
+def inout(request, monkeypatch, capsys):
+    request.cls.monkeypatch = monkeypatch
+    request.cls.capsys = capsys
+
+@pytest.mark.usefixtures("inout")
 class StateTestCases(TestCase):
     def test_saving_account(self):
         newState = State()
+        try:
+            os.remove('accountsTest.csv')
+        except:
+            pass
 
-        newState.account_file_name = "accounts.csv"
+        newState.account_file_name = "accountsTest.csv"
         newState.users["UserName1"] = User("first","second","Username1", "Password1!")
         assert newState.save_accounts() == True
+        os.remove('accountsTest.csv')
 
     def test_saving_over_five_accounts(self):
         newState = State()
 
-        newState.account_file_name = "accounts.csv"
+        try:
+            os.remove('accountsTest.csv')
+        except:
+            pass
+
+        newState.account_file_name = "accountsTest.csv"
         newState.users["UserName1"] = User("first","second","Username1", "Password1!")
         newState.users["UserName2"] = User("first","second","Username2", "Password1!")
         newState.users["UserName3"] = User("first","second","Username3", "Password1!")
@@ -23,8 +43,30 @@ class StateTestCases(TestCase):
         newState.users["UserName5"] = User("first","second","Username5", "Password1!")
         newState.users["UserName6"] = User("first","second","Username6", "Password1!")
         newState.save_accounts()
+        assert newState.save_accounts() == True
+        os.remove('accountsTest.csv')
+
+    def test_saving_over_five_accounts(self):
+        newState = State()
+        try:
+            os.remove('accountsTest.csv')
+        except:
+            pass
+        newState.account_file_name = "accountsTest.csv"
+        newState.users["UserName1"] = User("first","second","Username1", "Password1!")
+        newState.users["UserName2"] = User("first","second","Username2", "Password1!")
+        newState.users["UserName3"] = User("first","second","Username3", "Password1!")
+        newState.users["UserName4"] = User("first","second","Username4", "Password1!")
+        newState.users["UserName5"] = User("first","second","Username5", "Password1!")
+        newState.users["UserName6"] = User("first","second","Username6", "Password1!")
+        newState.users["UserName7"] = User("first","second","Username7", "Password1!")
+        newState.users["UserName8"] = User("first","second","Username8", "Password1!")
+        newState.users["UserName9"] = User("first","second","Username9", "Password1!")
+        newState.users["UserName10"] = User("first","second","Username10", "Password1!")
+        newState.users["UserName11"] = User("first","second","Username11", "Password1!")
+        newState.save_accounts()
         assert newState.save_accounts() == False
-    
+
     def test_contains_capital_letter(self):
         assert contains_capital_letter('Squeaky1!') == True
         assert contains_capital_letter('squEaky1!') == True
@@ -81,7 +123,7 @@ class StateTestCases(TestCase):
 
     def test_user_list(self):
         user = User("first","second","Username1", "Password1!")
-        assert user.list() == ["first","second","Username1", "Password1!",""]
+        assert user.list() == ["first","second","Username1","Password1!","",True,True,True,0,"",""]
 
     def test_user_success_story(self):
         user = User("first","second","Username1", "Password1!")
@@ -95,3 +137,37 @@ class StateTestCases(TestCase):
         assert job.employer == "Apple"
         assert job.location == "USA"
         assert job.salary == "$100000"
+    def test_change_langauge(self):
+        user = User("first","second","Username1", "Password1!")
+        user.change_language(0)
+        assert user.language == 0
+
+    def test_search(self):
+        newState = State()
+        searchPage = SearchStudents(state=newState)
+        newState.users["user1"] = User('Sophia','Jones','sophie','password1',major = 'Computer Science',university='University of South Florida')
+        newState.current_user = newState.users["user1"]
+        newState.users["user2"] = User('John','Dinkleburg','dinkster','password2',major = 'Interpretive Dance',university='University of Central Florida')
+        newState.users["user3"] = User('Bruce','Wayne','notbatman','password3',major = 'Business Administration',university='Florida Gulf Coast University')
+        newState.users["user4"] = User('Peppino','Pizza','pizzafiend','password4',major = 'Culinary Arts',university='School of Hard Knocks')
+        newState.users["user5"] = User('Bevin','Barker','angryimpala','password5',major = 'Music Theory',university='University of South Florida')
+        assert searchPage.is_exists(lastname='Wayne') == newState.users["user3"]
+        assert searchPage.is_exists(lastname = "Jones") == None
+        assert searchPage.is_exists(lastname = 'Hoeke') == None
+        assert searchPage.is_exists(university='Florida Gulf Coast University') == newState.users["user3"]
+        assert searchPage.is_exists(university='Harvard University') == None
+        assert searchPage.is_exists(major = 'Culinary Arts') == newState.users["user4"]
+        assert searchPage.is_exists(major = "Mechanical Engineering") == None
+
+    def test_friend_requests(self):
+        self.monkeypatch.setattr('sys.stdin', io.StringIO('\n'))
+        newState = State()
+        searchPage = SearchStudents(state=newState)
+        newState.users["sophie"] = User('Sophia','Jones','sophie','password1',major = 'Computer Science',university='University of South Florida')
+        newState.current_user = newState.users["sophie"]
+        newState.users["dinkster"] = User('John','Dinkleburg','dinkster','password2',major = 'Interpretive Dance',university='University of Central Florida')
+        searchPage.send_request(newState.users["dinkster"].username)
+        output = self.capsys.readouterr()
+        #test that both users have the pending requests.
+        assert newState.users["dinkster"].username in newState.current_user.sent_requests
+        assert newState.current_user.username in newState.users["dinkster"].pending_requests
