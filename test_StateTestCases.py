6a4f96311638f1f0ee00c320230ef0d4f4314ed2
Ty Desktop <typiesco@gmail.com>
2023-03-07 18:59:56 -0500

    Fixed broken test due to new user fields

--- test_StateTestCases.py
+++ test_StateTestCases.py
@@ -8,10 +8,12 @@ from User import *
 from Util import *
 from Job import *
 
+
 @pytest.fixture(scope="function")
 def inout(request, monkeypatch, capsys):
     request.cls.monkeypatch = monkeypatch
     request.cls.capsys = capsys
+
 
 @pytest.mark.usefixtures("inout")
 class StateTestCases(TestCase):
@@ -24,7 +26,8 @@ class StateTestCases(TestCase):
 
         newState.account_file_name = "accountsTest.csv"
         newState.friends_file_name = 'friendsTest.csv'
-        newState.users["UserName1"] = User("first","second","Username1", "Password1!")
+        newState.users["UserName1"] = User(
+            "first", "second", "Username1", "Password1!")
         assert newState.save_accounts() == True
         os.remove('accountsTest.csv')
 
@@ -37,12 +40,18 @@ class StateTestCases(TestCase):
             pass
 
         newState.account_file_name = "accountsTest.csv"
-        newState.users["UserName1"] = User("first","second","Username1", "Password1!")
-        newState.users["UserName2"] = User("first","second","Username2", "Password1!")
-        newState.users["UserName3"] = User("first","second","Username3", "Password1!")
-        newState.users["UserName4"] = User("first","second","Username4", "Password1!")
-        newState.users["UserName5"] = User("first","second","Username5", "Password1!")
-        newState.users["UserName6"] = User("first","second","Username6", "Password1!")
+        newState.users["UserName1"] = User(
+            "first", "second", "Username1", "Password1!")
+        newState.users["UserName2"] = User(
+            "first", "second", "Username2", "Password1!")
+        newState.users["UserName3"] = User(
+            "first", "second", "Username3", "Password1!")
+        newState.users["UserName4"] = User(
+            "first", "second", "Username4", "Password1!")
+        newState.users["UserName5"] = User(
+            "first", "second", "Username5", "Password1!")
+        newState.users["UserName6"] = User(
+            "first", "second", "Username6", "Password1!")
         newState.save_accounts()
         assert newState.save_accounts() == True
         os.remove('accountsTest.csv')
@@ -54,17 +63,28 @@ class StateTestCases(TestCase):
         except:
             pass
         newState.account_file_name = "accountsTest.csv"
-        newState.users["UserName1"] = User("first","second","Username1", "Password1!")
-        newState.users["UserName2"] = User("first","second","Username2", "Password1!")
-        newState.users["UserName3"] = User("first","second","Username3", "Password1!")
-        newState.users["UserName4"] = User("first","second","Username4", "Password1!")
-        newState.users["UserName5"] = User("first","second","Username5", "Password1!")
-        newState.users["UserName6"] = User("first","second","Username6", "Password1!")
-        newState.users["UserName7"] = User("first","second","Username7", "Password1!")
-        newState.users["UserName8"] = User("first","second","Username8", "Password1!")
-        newState.users["UserName9"] = User("first","second","Username9", "Password1!")
-        newState.users["UserName10"] = User("first","second","Username10", "Password1!")
-        newState.users["UserName11"] = User("first","second","Username11", "Password1!")
+        newState.users["UserName1"] = User(
+            "first", "second", "Username1", "Password1!")
+        newState.users["UserName2"] = User(
+            "first", "second", "Username2", "Password1!")
+        newState.users["UserName3"] = User(
+            "first", "second", "Username3", "Password1!")
+        newState.users["UserName4"] = User(
+            "first", "second", "Username4", "Password1!")
+        newState.users["UserName5"] = User(
+            "first", "second", "Username5", "Password1!")
+        newState.users["UserName6"] = User(
+            "first", "second", "Username6", "Password1!")
+        newState.users["UserName7"] = User(
+            "first", "second", "Username7", "Password1!")
+        newState.users["UserName8"] = User(
+            "first", "second", "Username8", "Password1!")
+        newState.users["UserName9"] = User(
+            "first", "second", "Username9", "Password1!")
+        newState.users["UserName10"] = User(
+            "first", "second", "Username10", "Password1!")
+        newState.users["UserName11"] = User(
+            "first", "second", "Username11", "Password1!")
         newState.save_accounts()
         assert newState.save_accounts() == False
 
@@ -76,7 +96,6 @@ class StateTestCases(TestCase):
     def test_contains_small_letter(self):
         assert contains_capital_letter('squeaky1!') == False
         assert contains_capital_letter('squaey1!') == False
-
 
     def test_contains_number(self):
         assert contains_number('Squeaky0!') == True
@@ -91,7 +110,6 @@ class StateTestCases(TestCase):
         assert contains_number('Squeak9y!') == True
         assert contains_number('Squeaky!') == False
 
-
     def test_contains_special_character(self):
         assert contains_special_character('Squeaky1!') == True
         assert contains_special_character('Sq@ueaky1') == True
@@ -104,30 +122,29 @@ class StateTestCases(TestCase):
         assert contains_special_character('S(queaky1') == True
         assert contains_special_character('Squeaky1') == False
 
-
     def test_validate_password(self):
         assert validate_password('Squeaky1!') == True
         assert validate_password('squeaky2!') == False
         assert validate_password('Squeaky!') == False
         assert validate_password('Squeaky3') == False
 
-
     def test_user_password(self):
-        user = User("first","second","Username1", "Password1!")
+        user = User("first", "second", "Username1", "Password1!")
         assert user.password == "Password1!"
         assert user.password != "Password1"
 
     def test_change_password(self):
-        user = User("first","second","Username1", "Password1!")
+        user = User("first", "second", "Username1", "Password1!")
         user.change_password("Password12@@")
         assert user.password == "Password12@@"
 
     def test_user_list(self):
-        user = User("first","second","Username1", "Password1!")
-        assert user.list() == ["first","second","Username1","Password1!","",True,True,True,0,"",""]
+        user = User("first", "second", "Username1", "Password1!")
+        assert user.list() == ["first", "second", "Username1", "Password1!",
+                               "", True, True, True, 0, "", "", "", "", "", "", "", ""]
 
     def test_user_success_story(self):
-        user = User("first","second","Username1", "Password1!")
+        user = User("first", "second", "Username1", "Password1!")
         user.set_success_story("Testing out this functionality")
         assert user.success_story == "Testing out this functionality"
 
@@ -138,27 +155,36 @@ class StateTestCases(TestCase):
         assert job.employer == "Apple"
         assert job.location == "USA"
         assert job.salary == "$100000"
+
     def test_change_langauge(self):
-        user = User("first","second","Username1", "Password1!")
+        user = User("first", "second", "Username1", "Password1!")
         user.change_language(0)
         assert user.language == 0
 
     def test_search(self):
         newState = State()
         searchPage = SearchStudents(state=newState)
-        newState.users["user1"] = User('Sophia','Jones','sophie','password1',major = 'Computer Science',university='University of South Florida')
+        newState.users["user1"] = User('Sophia', 'Jones', 'sophie', 'password1',
+                                       major='Computer Science', university='University of South Florida')
         newState.current_user = newState.users["user1"]
-        newState.users["user2"] = User('John','Dinkleburg','dinkster','password2',major = 'Interpretive Dance',university='University of Central Florida')
-        newState.users["user3"] = User('Bruce','Wayne','notbatman','password3',major = 'Business Administration',university='Florida Gulf Coast University')
-        newState.users["user4"] = User('Peppino','Pizza','pizzafiend','password4',major = 'Culinary Arts',university='School of Hard Knocks')
-        newState.users["user5"] = User('Bevin','Barker','angryimpala','password5',major = 'Music Theory',university='University of South Florida')
-        assert searchPage.is_exists(lastname='Wayne') == newState.users["user3"]
-        assert searchPage.is_exists(lastname = "Jones") == None
-        assert searchPage.is_exists(lastname = 'Hoeke') == None
-        assert searchPage.is_exists(university='Florida Gulf Coast University') == newState.users["user3"]
+        newState.users["user2"] = User('John', 'Dinkleburg', 'dinkster', 'password2',
+                                       major='Interpretive Dance', university='University of Central Florida')
+        newState.users["user3"] = User('Bruce', 'Wayne', 'notbatman', 'password3',
+                                       major='Business Administration', university='Florida Gulf Coast University')
+        newState.users["user4"] = User('Peppino', 'Pizza', 'pizzafiend', 'password4',
+                                       major='Culinary Arts', university='School of Hard Knocks')
+        newState.users["user5"] = User('Bevin', 'Barker', 'angryimpala', 'password5',
+                                       major='Music Theory', university='University of South Florida')
+        assert searchPage.is_exists(
+            lastname='Wayne') == newState.users["user3"]
+        assert searchPage.is_exists(lastname="Jones") == None
+        assert searchPage.is_exists(lastname='Hoeke') == None
+        assert searchPage.is_exists(
+            university='Florida Gulf Coast University') == newState.users["user3"]
         assert searchPage.is_exists(university='Harvard University') == None
-        assert searchPage.is_exists(major = 'Culinary Arts') == newState.users["user4"]
-        assert searchPage.is_exists(major = "Mechanical Engineering") == None
+        assert searchPage.is_exists(
+            major='Culinary Arts') == newState.users["user4"]
+        assert searchPage.is_exists(major="Mechanical Engineering") == None
 
     def test_friend_requests(self):
         self.monkeypatch.setattr('sys.stdin', io.StringIO('\n'))
@@ -166,29 +192,31 @@ class StateTestCases(TestCase):
         newState.friends_file_name = 'friendsTest.csv'
         searchPage = SearchStudents(state=newState)
         friendsPage = Friends(state=newState)
-        newState.users["sophie"] = User('Sophia','Jones','sophie','password1',major = 'Computer Science',university='University of South Florida')
+        newState.users["sophie"] = User('Sophia', 'Jones', 'sophie', 'password1',
+                                        major='Computer Science', university='University of South Florida')
         newState.current_user = newState.users["sophie"]
-        newState.users["dinkster"] = User('John','Dinkleburg','dinkster','password2',major = 'Interpretive Dance',university='University of Central Florida')
+        newState.users["dinkster"] = User('John', 'Dinkleburg', 'dinkster', 'password2',
+                                          major='Interpretive Dance', university='University of Central Florida')
         searchPage.send_request(newState.users["dinkster"].username)
         output = self.capsys.readouterr()
-        #test that both users have the pending requests.
+        # test that both users have the pending requests.
         assert newState.users["dinkster"].username in newState.current_user.sent_requests
         assert newState.current_user.username in newState.users["dinkster"].pending_requests
 
-        #test accept_friend_request
+        # test accept_friend_request
         newState.current_user = newState.users["dinkster"]
         friendsPage.accept_request("sophie")
         output = self.capsys.readouterr()
         assert "sophie" in newState.current_user.friends
         assert "dinkster" in newState.users["sophie"].friends
 
-        #test remove_friend
+        # test remove_friend
         friendsPage.remove_friend("sophie")
         output = self.capsys.readouterr()
         assert not ("sophie" in newState.current_user.friends)
         assert not ("dinkster" in newState.users["sophie"].friends)
 
-        #test decline request
+        # test decline request
         searchPage.send_request(newState.users["sophie"].username)
 
         newState.current_user = newState.users["sophie"]
@@ -196,5 +224,7 @@ class StateTestCases(TestCase):
         output = self.capsys.readouterr()
         assert not ("dinkster" in newState.current_user.friends)
         assert not ("sophie" in newState.users["dinkster"].friends)
-        assert not (newState.users["dinkster"].username in newState.current_user.sent_requests)
-        assert not (newState.current_user.username in newState.users["dinkster"].pending_requests)
+        assert not (
+            newState.users["dinkster"].username in newState.current_user.sent_requests)
+        assert not (
+            newState.current_user.username in newState.users["dinkster"].pending_requests)
